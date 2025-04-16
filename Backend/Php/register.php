754bea65d877bd62b7ca0cf7b5d5b11661d3866b
Bintu <bjalloh@smith.edu>
2025-04-16 15:50:41 -0400

    second commit

---
+++ Backend/Php/register.php
@@ -0,0 +1,8 @@
+// This file will accept POST data from the register form (like name, email, password).
+// Validate to make sure the email isn't already registered and Sanitize the data 
+// Save the new user into the users.json file
+<?php
+
+
+
+>?
